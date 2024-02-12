b1c0aaa2f54825b1fc7e38ce7e1583ec86b82933
yashvi-dave <yashvidave03@gmail.com>
2024-02-12 22:48:56 +0530

    week 5

--- week5/js/script.js
+++ week5/js/script.js
@@ -83,9 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
-});
+true);});
+   // ***** <---- TODO: STEP 1: Substitute [...]   [...] ******
+  // Explicitly setting the flag to get JSON from server processed into an object literal
+
 // *** finish **
 
 
