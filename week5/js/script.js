d9f16e62b65f1f43d9d9405295f7df05a23d0053
yashvi-dave <149178319+yashvi-dave@users.noreply.github.com>
2024-02-14 10:54:09 +0530

    Update script.js

--- week5/js/script.js
+++ week5/js/script.js
@@ -15,13 +15,13 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
- // "https://davids-restaurant.herokuapp.com/categories.json";
+ "https://davids-restaurant.herokuapp.com/categories.json";
   //"https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  //"https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-  "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json?category="
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  //"https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json?category="
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
